720a951e47f9c5c3760347eded821fb7a98e1b87
l-phan <l-phan@creationline.com>
2025-04-08 08:05:33 +0900

    add connect to inventory service

--- src/main/java/com/loc/microservices/order_service_spring_boot/service/OrderService.java
+++ src/main/java/com/loc/microservices/order_service_spring_boot/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.loc.microservices.order_service_spring_boot.client.InventoryClient;
 import com.loc.microservices.order_service_spring_boot.dto.OrderRequest;
 import com.loc.microservices.order_service_spring_boot.model.Order;
 import com.loc.microservices.order_service_spring_boot.repository.OrderRepository;
@@ -15,14 +16,25 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        
+
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product with Skucode: " + orderRequest.skuCode() + " is not in stock");
+        }
+        
+
+        
     }
 }
