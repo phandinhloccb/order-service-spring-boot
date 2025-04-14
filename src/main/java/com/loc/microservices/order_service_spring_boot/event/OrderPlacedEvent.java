ff72bd285c16501d7b8ab4655e3a4d84131b1905
l-phan <l-phan@creationline.com>
2025-04-14 21:37:40 +0900

    remove not need import

--- src/main/java/com/loc/microservices/order_service_spring_boot/event/OrderPlacedEvent.java
+++ src/main/java/com/loc/microservices/order_service_spring_boot/event/OrderPlacedEvent.java
@@ -1,6 +1,5 @@
 package com.loc.microservices.order_service_spring_boot.event;
 
-import org.apache.avro.generic.GenericArray;
 import org.apache.avro.specific.SpecificData;
 import org.apache.avro.util.Utf8;
 import org.apache.avro.message.BinaryMessageEncoder;
