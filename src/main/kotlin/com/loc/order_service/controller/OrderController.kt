602cb8acc30d793d5e8c76339709ed7d952782b6
Phan Dinh Loc <101810548+phandinhloccb@users.noreply.github.com>
2026-01-18 16:42:11 +0900

    AI patch for issue #151

--- src/main/kotlin/com/loc/order_service/controller/OrderController.kt
+++ src/main/kotlin/com/loc/order_service/controller/OrderController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/orders")
-class OrderController(
+class OrderController @Autowired constructor(
     private val orderService: OrderService
 ) {
     @PostMapping
